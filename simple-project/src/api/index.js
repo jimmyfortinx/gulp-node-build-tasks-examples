19b7bee469a9d8e23011196ea2640cd5fe608864
jimmyfortin <jimmy.fortin@corp.ovh.com>
2015-12-28 09:12:14 -0500

    New find by firstname api method

--- simple-project/src/api/index.js
+++ simple-project/src/api/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var controller = require('./api.controller');
 
 router.get('/awesome-list', controller.getAwesomeList);
+router.get('/awesome-list/find/firstname/:firstname', controller.findFirstname);
 
 module.exports = router;
