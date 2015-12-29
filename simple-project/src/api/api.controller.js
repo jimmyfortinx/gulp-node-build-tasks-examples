9f6c31518e3ee60fc2e90b216af82de0df470c48
jimmy.fortin@gmail.com <jimmy.fortin@gmail.com>
2015-12-29 10:09:05 -0500

    jshint and jscs added to simple project and rules applied

--- simple-project/src/api/api.controller.js
+++ simple-project/src/api/api.controller.js
@@ -1,16 +1,16 @@
-var _ = require("lodash");
+var _ = require('lodash');
 
 var people = [
-    { firstname: "John", lastname: "Smith" },
-    { firstname: "John", lastname: "Smith2" },
-    { firstname: "Peter", lastname: "Pan" },
-    { firstname: "Gino", lastname: "Chouinard" }
+    { firstname: 'John', lastname: 'Smith' },
+    { firstname: 'John', lastname: 'Smith2' },
+    { firstname: 'Peter', lastname: 'Pan' },
+    { firstname: 'Gino', lastname: 'Chouinard' }
 ];
 
-exports.getAwesomeList = function (req, res) {
+exports.getAwesomeList = function(req, res) {
     res.json(people);
 };
 
-exports.findFirstname = function (req, res) {
+exports.findFirstname = function(req, res) {
     res.json(_.where(people, { firstname: req.params.firstname }));
-}
+};
--- simple-project/src/server.js
+++ simple-project/src/server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send("This server is running, your can get an api example at: /api/awesome-list")
+app.get('/', function(req, res) {
+    res.send('This server is running, your can get an api example at: ' +
+                '/api/awesome-list');
 });
 
 app.use('/api', require('./api'));
 
-var server = app.listen(9000, function () {
+var server = app.listen(9000, function() {
     var host = server.address().address;
     var port = server.address().port;
 
