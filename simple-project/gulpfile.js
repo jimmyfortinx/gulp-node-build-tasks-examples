435210985ee555710ce9a7ec1e85bc63241504a4
jimmyfortin <jimmy.fortin@corp.ovh.com>
2016-01-29 15:58:32 -0500

    projectDirectory defined to fixed wrong directory problem

--- simple-project/gulpfile.js
+++ simple-project/gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 var gulpNodeBuildTasks = require('gulp-node-build-tasks');
 
+var config = {
+    // Userful only because for testing we are using npm link,
+    // otherwise you can remove it
+    projectDirectory: __dirname
+}
+
 gulpNodeBuildTasks.use(gulp);
+gulpNodeBuildTasks.configure(config);
 gulpNodeBuildTasks.registerTasks();
