72575c1afe53a3660f6c42846931dff836ef371f
jimmy.fortin@gmail.com <jimmy.fortin@gmail.com>
2016-02-15 16:56:03 -0500

    Traces are shown on failed unit tests

--- simple-project/gulpfile.js
+++ simple-project/gulpfile.js
@@ -4,7 +4,11 @@ var gulpNodeBuildTasks = require('gulp-node-build-tasks');
 var config = {
     // Userful only because for testing we are using npm link,
     // otherwise you can remove it
-    projectDirectory: __dirname
+    projectDirectory: __dirname,
+
+    unitTest: {
+        showTrace: true
+    }
 }
 
 gulpNodeBuildTasks.apply(config, gulp);
